174fe2751b8cb48202dbb26f754bffeb028313ae
jinwu <ye.xing@ecar.com>
2023-06-12 14:48:10 +0800

    修改文件夹名字

--- src/main/java/com/nijigasaki/knowledge/ServiceApplication.java
+++ src/main/java/com/nijigasaki/knowledge/ServiceApplication.java
@@ -1,4 +1,4 @@
-package com.nijigasaki.knowledge.service;
+package com.nijigasaki.knowledge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
